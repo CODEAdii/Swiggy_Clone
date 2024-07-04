b96cba2af8bfb554244644d0e43b67bb3a6c6df7
Adi <adityamauryamaurya144@gmail.com>
2024-07-04 23:49:58 +0545

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import { useState } from 'react';
 import { RxCaretDown } from "react-icons/rx";
+import { CiSearch } from "react-icons/ci";
+import { BiSolidOffer } from "react-icons/bi";
+import { BsCartPlus } from "react-icons/bs";
+import { VscAccount } from "react-icons/vsc";
+
+
+
+
 
 
 const Header = () => {
@@ -9,29 +17,70 @@ const Header = () => {
     const showSideMenu=()=>{
         setToggle(true);
     }
+    const hideSideMenu=()=>{
+        setToggle(false);
+    }
+
+    const links=[
+        {
+            icon:<CiSearch/>,
+            name:"Search"
+        },{
+            icon:<BiSolidOffer/>,
+            name:"Offers"
+        },{
+            icon:"",
+            name:"Help"
+        },{
+            icon:<BsCartPlus/>,
+            name:"Cart"
+        }
+        ,{
+            icon:<VscAccount/>,
+            name:"SignIn"
+        }
+    ]
+    
   return (
     <>
-     <div className='black-overlay w-full h-full fixed duration-500'
+     <div className='black-overlay w-full h-full fixed duration-500' onClick={hideSideMenu}
       style={{ 
-        opacity:toggle ? 1:0,
+        opacity:toggle ? 1 : 0,
         visibility:toggle ?'visible':'hidden'}}>
-            <div className='w-[50px] bg-white h-full absolute'
+            <div onClick={(e)=> {
+                e.stopPropagation();
+            }} className='w-[500px] bg-white h-full absolute duration-[400ms]'
             style={{
 
-                oppacity:toggle? '0%' :'-100% '
+                left:toggle? '0%' :'-100% '
             }}>
 
             </div>
 
         </div>
         <header className="p-[15px] shadow-xl text-[#686b78]">
-            <div className="max-w-[1200px] mx-auto border border-red-500 flex items-center">
+            <div className="max-w-[1200px] mx-auto flex items-center">
                 <div className='w-[100px] '>
                     <img src="images/Swiggy_logo.png" alt="" className='w-full' />
                 </div>
                 <div>
                     <span className='font-bold border-b-[3px] border-[#686b78] '> Kumarpara </span>Guwahati, Assam, India <RxCaretDown fontSize={25} className='inline text-[#fc8019] cursor-pointer' onClick={showSideMenu}/>
                 </div>
+                <nav className='flex list-none gap-10 ml-auto font-bold text-[18px]'>
+                    {
+                        links.map(
+                            (link,index)=>{
+                                 return <li key={index} className='flex items-center gap-2 cursor-pointer hover:text-[#fc8019]'>
+                                    {link.icon}  
+                                    {link.name}
+                                     
+                                </li>
+                            }
+                        )
+                    }
+               
+            
+                </nav>
             </div> 
         </header>
      
