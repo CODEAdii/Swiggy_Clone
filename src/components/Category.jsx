b8822cdc84789a866cfb0916927b4117e02157e2
Adi <adityamauryamaurya144@gmail.com>
2024-07-05 01:12:08 +0530

    third  commit

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { FaArrowRight,FaArrowLeft } from "react-icons/fa6";
+
+
+
 
 const Category = () => {
+    const [categories,setCategory]=useState([]);
+
+    const fetchCategory=async()=>{
+            const response=await fetch("http://localhost:5000/categories");
+            const data = await response.json();
+            setCategory(data);
+    }
+
+    useEffect( 
+        () =>{
+        fetchCategory();
+    })
   return (
-     <div className=' max-w-[1200px] mx-auto flex items-center'>
-        <div>
-            <div>What's on your mind?</div>
-            <div className='flex items-center justify-between'>
-                <div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2'></div>
-                <div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2'></div>
+     <div className=' max-w-[1200px] mx-auto '>
+        <div className='flex my-3 items-center justify-between'>
+            <div className='text-[25px] font-bold'>What's on your mind?</div>
+            <div className='flex '>
+                <div className='flex justify-center items-center w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 cursor-pointer'><FaArrowRight/></div>
+                <div className='flex justify-center items-center w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 cursor-pointer'><FaArrowLeft/></div>
 
             </div>
+        </div>
+        <div className='flex border border-red-500 overflow-hidden'>
+            {
+                categories.map(
+                    (cat,index)=>{
+                        return(
+                            <div key={index} className='w-[150px] shrink-0'>
+                                <img src={"http://localhost:5000/images/"+ cat.image} alt=""/>
+                            </div>
+                        )    
+                    }
+                )
+            }
         </div>
      </div>
      )    
